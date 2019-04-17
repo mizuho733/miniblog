200d74b38f3fec1cdc8d1132da1d88d33fa0d34b
Mizuho Isogai <isogai@isogaimizuhonoMacBook-puro.local>
2019-04-17 18:11:29 +0900

    edit destroyアクションの追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
   resources :users, only: [:index, :edit, :update]
-  resources :posts, only: [:index, :new, :create]
+  resources :posts, only: [:index, :new, :create, :edit, :destroy]
 end
