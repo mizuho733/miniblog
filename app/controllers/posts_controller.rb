d748650e6dfb631b8b52d4521bf88704bb3c7085
Mizuho Isogai <isogai@isogaimizuhonoMacBook-puro.local>
2019-04-18 21:32:15 +0900

    新規投稿にエラー文表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,12 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(post_params)
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
   end
 
   def destroy
